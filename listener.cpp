aa37cf793fab8c25991a31ed62c9ae04616aa6d4
Weston Thornburg <lamlatx@gmail.com>
2012-11-04 12:01:16 -0800

    fixed some syntax making progress s:life is pain

--- listener.cpp
+++ listener.cpp
@@ -2,77 +2,90 @@
 #include <tuple>
 #include "RF.h";
 #include <Packet.h>;
+#include "CircularFifo.h" 
 using namespace std;
 
-// taking a blind swing at some of things we will want in this bad boy not sure I have all my shit strait
-//I have no idea how to properly do a constructor in relation to the whole .h stuff I figure this is shit we can work out
-int main(int argc, char const *argv[])
+
+Listener::Listener(RF* RFLayer, CircularFifo<int,10>* incomingQueue, unsigned short* sendFlag, bool* receivedFlag, unsigned short myMAC
 {
-	daRF->attachThread();// attach the thread
-	MACACK = 0;//special case no need to send an ACK
-	ack_Received = false;// indicates if an ack comes in
+    daRF = RFLayer;//our reference to the RF layer
+    MACACK = sendFlag;
+    MACACK = 0;//special case no need to send an ACK
+    ack_Received = recivedFlag;
+    ack_Received = false;// indicates if an ack comes in
+    MACaddr = myMAC;
+    char buf[MAXPACKETSIZE];// buffer for the incoming packets
+    daLine = incomingQueue;
 
 }
 
-int ultraListen()
+int
+Listener::ultraListen()
 {
     while(true){
-            // get the bytes received
-        bytesReceived = daRF->receive(buf, MAXPACKETSIZE);
+        int bytesReceived;// bytes from the last packet
+        int PRR;//Packet Read Result
+        // wait for data
+        bytesReceived = daRF->receive(buf, MAXPACKETSIZE);//block until data comes our way
             //print the bytes received and checks for errors
         if (bytesReceived != MAXPACKETSIZE){
             wcerr << "Received  partial Packet with " << bytesReceived << " bytes of data!" << endl;
         }
         else{
-            wcerr << bytesReceived << " !Full Packet Received!  ";
+            wcerr << bytesReceived << " !Full Packet Received! woo! ";
         }
         PRR = read_Packet();
         if (PRR == 1)//if the packet is relevent to us and is data queue it up
         {
-        	status = queue_data();//put data in the fifo
+            status = queue_data();//put data in the fifo
         }
-        if (PRR == 2)//if the packet is relevent and is an ACK ajust ack recived flag
+        if (PRR == 2)//if the packet is relevent and is an ACK adjust ack recived flag
         {
-        	ack_Received == true;
+            ack_Received = true;
         }
     }
 }
 
-int read_Packet ()
+int
+Listener::read_Packet ()
 {
-	int status;//will be returned with different status code to help ultra listen react 
-	short packetDest = buf[2];//bitwise terribleness
-	packetDest << 8;
-	packetDest = packetDest + buf[3];
-	if (packetDest != MACaddr)//compare the destination of this packet to our MAC address
-	{
-		status = 0;//this packet isn't for us
-		wcerr << "Packet not addressed to current MAC address." << endl;
-		return status;
-	}
-	char frameType = buf[0];
-	frameType >> 5;
-	switch (frameType)//compare the frame type of the packet given to known types to figure out what kind of packet it is
-	{
-		case 0:
-			wcerr << "Data packet received." << endl;
-			status = 1;
-			break;
+    int status;//will be returned with different status code to help ultra listen react 
+    short packetDest = buf[2];//bitwise terribleness
+    packetDest << 8;
+    packetDest = packetDest + buf[3];
+    if (packetDest != MACaddr)//compare the destination of this packet to our MAC address
+    {
+        status = 0;//this packet isn't for us
+        wcerr << "Packet not addressed to current MAC address." << endl;
+        return status;
+    }
+    char frameType = buf[0];
+    frameType >> 5;
+    switch (frameType)//compare the frame type of the packet given to known types to figure out what kind of packet it is
+    {
+        case 0:
+            wcerr << "Data packet received." << endl;
+            short dataSource = buf[4];//extract the source address
+            dataSource << 8;
+            dataSource = dataSource + buf[5];
+            MACACK = dataSource;//let the sender know to send an ACK for this data
+            status = 1;
+            break;
 
-		case 1:
-			wcerr << "ACK Received." << endl;
-			status = 2;
-			break;
+        case 1:
+            wcerr << "ACK Received." << endl;
+            status = 2;
+            break;
 
-		case 2:
-			wcerr << "Beacon Received." << endl;
-			status = 3;
-			break;
+        case 2:
+            wcerr << "Beacon Received." << endl;
+            status = 3;
+            break;
 
-		default:
-			wcerr << "Unknown packet type received." << endl;
-			status = 3;
-			break;
-	}
-	return status;
+        default:
+            wcerr << "Unknown packet type received." << endl;
+            status = 3;
+            break;
+    }
+    return status;
 }
--- listener.h
+++ listener.h
@@ -5,55 +5,45 @@
  * This means either alerting the sender the status of ACK(ours or others), or saving data for our lordly demiBrad.
  */
 
-#include <iostream>;
-#include "RF.h";
-#include <Packet.h>;
-#include "CircularFifo.h" 
-using namespace std;
-
 class Listener
 {
 public:
 
-	/*
-	 * constructor for the listener class that sets up all our sexy varribles and
-	 * starts the thread listening for imcoming messages
-	 */
-	Listener();
+    /*
+     * constructor for the listener class that sets up all our sexy varribles and
+     * starts the thread listening for imcoming messages
+     */
+    Listener();
 
 private:
 
-	typedef tuple <short, int, char>// the tupal for handing to the layer above
-	*short MACaddr; //a pointer to our MAC address
-	*ostream streamy; //the given output stream for data to the layer above
-	*short MACACK;//a pointer to the MAC address of the most recent sender of data that has not been sent an ACK yet
-	// or assuming that none need to be sent a special case of zero should be used to indicate this
-	*queue incoming_Queue <short, char, int>;//pointer to the queue for incoming data to be held until the layer above ask for data
-	*bool ack_Received;// a pointer to a boolean that indicates whether or not a ACK has been recived 
-	char buf[MAXPACKETSIZE];// buffer for the incoming packet
-	int bytesReceived;// bytes from the last packet
-	int PRR;//short for Packed Read Result
-	const int MAXPACKETSIZE = 2048; //size guarenteed to hold all properly formated packets
-	RF* daRF = new RF();//our reference to the RF layer
+    typedef tuple <short, int, char>// the tupal for handing to the layer above
+    *short MACaddr; //a pointer to our MAC address
+    *ostream streamy; //the given output stream for data to the layer above
+    *short MACACK;//a pointer to the MAC address of the most recent sender of data that has not been sent an ACK yet
+    // or assuming that none need to be sent a special case of zero should be used to indicate this
+    *bool ack_Received;// a pointer to a boolean that indicates whether or not a ACK has been recived 
+    static const int MAXPACKETSIZE = 2048; //size guarenteed to hold all properly formated packets
+    
 
 
-	/*
-	 * the heart of the listener watches activity on the RF layer and blocks until a packet is recived
-	 */
-	int UltraListen();
+    /*
+     * the heart of the listener watches activity on the RF layer and blocks until a packet is recived
+     */
+    int UltraListen();
 
-	/*
-	 * looks at a packet to check for three things from every packet that comes across the the RF layer
-	 * first it makes sure the packet conforms to the 802.11~ specs if not it disregards it
-	 * second it looks to see if the packet is addressed to our MAC address if not it disregards it 
-	 * finally if the packet is for us it determines if it is an ACK or incoming data responding accordingly
-	 */
-	 int read_packet();
+    /*
+     * looks at a packet to check for three things from every packet that comes across the the RF layer
+     * first it makes sure the packet conforms to the 802.11~ specs if not it disregards it
+     * second it looks to see if the packet is addressed to our MAC address if not it disregards it 
+     * finally if the packet is for us it determines if it is an ACK or incoming data responding accordingly
+     */
+     int read_packet();
 
-	 /*
-	  * if an incoming packet is for us and has data for us then this method packs the relevent information,
-	  * the sender's MAC, an array of characters of the data, and the size of said array into a tupal that
-	  * then is put into our incoming_Queue\
-	  */
-	  int queue_data();
+     /*
+      * if an incoming packet is for us and has data for us then this method packs the relevent information,
+      * the sender's MAC, an array of characters of the data, and the size of said array into a tupal that
+      * then is put into our incoming_Queue\
+      */
+      int queue_data();
 };
