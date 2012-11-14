43c377e412d5e9127bc63f194be0d4dae3f023e8
Paul Makl <paul@paulnmakl.com>
2012-11-14 01:24:50 -0800

    getting there...

--- DemiBradTestMain.cpp
+++ DemiBradTestMain.cpp
@@ -14,34 +14,6 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	CircularFifo<Packet*, 3> queue_one;
-	CircularFifo<Packet*, 3> * pointer_to_queue_one = &queue_one;
-	(*pointer_to_queue_one).isEmpty();
-
-
-
-	const int bytes_to_send = 5;
-	char data[bytes_to_send];
-	// fill in the data array with dummy data
-	data[0] = 'A';
-	data[1] = 'B';
-	data[2] = 'C';
-	data[3] = 'D';
-	data[4] = 'E';
-	// create a pointer to the data
-	char* test = &data[0];
-	// create a packet
-	Packet packet_one;
-	packet_one.initPacket(3,true,5,257,514,test,67372036,bytes_to_send);
-	Packet * packpoint = &packet_one;
-	(*pointer_to_queue_one).push(packpoint);
-	Packet * packet_pointer;
-	//packet_pointer.initPacket(3,true,5,257,514,test,3,bytes_to_send);
-	(*pointer_to_queue_one).pop(packet_pointer);
-	Packet imback = *packet_pointer;
-	//cout << packet_pointer.get_crc() << "\n";*/
-
-
 	short MACaddrr = 3; // users mac address
 	ostream* streamyy; // provided ostream
 
