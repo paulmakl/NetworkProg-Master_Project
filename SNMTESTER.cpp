ce6f4127a063462b11863f675edb0458190596d5
Erin Jamroz <erinjamroz@gmail.com>
2012-11-17 17:39:25 -0800

    fixed seqNumManager; s:stable

--- SNMTESTER.cpp
+++ SNMTESTER.cpp
@@ -5,15 +5,18 @@ using std::cout;
 using std::endl;
 
 int main(int argc, char* argv[]) {
-    SeqNumManager test = SeqNumManager(4);
+    unsigned short four = 4;
+    unsigned short one = 1;
+    unsigned short two = 2;
+    SeqNumManager test = SeqNumManager(four);
 
-    cout << "expecting -1 : " <<  test.getSeqNum(1) << endl;
+    cout << "expecting -1 : " <<  test.getSeqNum(one) << endl;
     test.increment(2);
-    cout << "expecting 0 : " << test.getSeqNum(2) << endl;
+    cout << "expecting 0 : " << test.getSeqNum(two) << endl;
     cout << "expecting counting up to 0 : " << endl;
     for (int i=0; i<5; i++) {
-        test.increment(2);
-        cout << test.getSeqNum(2) << endl;
+        test.increment(two);
+        cout << test.getSeqNum(two) << endl;
     }
     return 1;
 }
--- SeqNumManager.h
+++ SeqNumManager.h
@@ -8,8 +8,9 @@
  * CSCI 325 University of Puget Sound
  */
 
-using namespace std;
+#include <iostream>
 #include <tr1/unordered_map>
+using std::tr1::unordered_map;
 
 
 class SeqNumManager {
@@ -24,17 +25,17 @@ class SeqNumManager {
          * initializes value for unseen MAC
          * param mac the MAC address to increment
          */
-        void increment(short mac);
+        void increment(unsigned short mac);
 
         /**
          * returns the sequence number for a given MAC address
          * param mac the MAC address to look up
          * return the seqNum or -1 is the address is not in list
          */
-        short getSeqNum(short mac);
+        short getSeqNum(unsigned short mac);
 
     private:
-        unordered_map<short, short> theMap; //Underlying data structure       
+        unordered_map<unsigned short, unsigned short> theMap; //Underlying data structure       
         unsigned short max; //Gets max sequence number
 };
 
@@ -43,11 +44,11 @@ SeqNumManager::SeqNumManager(unsigned short maxSeqNum) {
 }
 
 void
-SeqNumManager::increment(short mac) {
+SeqNumManager::increment(unsigned short mac) {
     short temp; //gets seqNum
     //check existance 
     if (theMap.count(mac)) {
-        if (theMap.at(mac) < max) {
+        if (theMap[mac] < max) {
             temp = theMap[mac] + 1; //Get previous seqNum and increment
             theMap.erase(mac);  //Get rid of previous value
             theMap[mac] = temp; //Set new incremented value
@@ -63,15 +64,14 @@ SeqNumManager::increment(short mac) {
 }
 
 short
-SeqNumManager::getSeqNum(short mac) {
+SeqNumManager::getSeqNum(unsigned short mac) {
     if (theMap.count(mac)) {
-        return theMap.at(mac);  //Return the sequNum
+        return theMap[mac];  //Return the sequNum
     }
     else {
         return -1;  //Address did not exist
     }
 }
-
 
 
 
